06134ec7fd4df7f2dde4c2c00961b5b243551b19
Asalagi <mypaintedfrog@yahoo.com>
2023-08-13 13:46:16 -0500

    restarting

--- server.js
+++ server.js
@@ -1,47 +1,24 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
-const port = 3001;
 
-const merchant_model = require('./merchant_model');
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-app.use(express.json());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-    next();
-});
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    merchant_model.getMerchants()
-    .then(response => {
-        res.status(200).send(response);
-    })
-    .catch(error => {
-    res.status(500).send(erro);
-    });
+  res.json({ message: "Welcome to your application." });
 });
 
-app.post("/merchants", (req, res) => {
-    merchant_model.createMerchant(req.body)
-    .then(response => {
-        res.statusMessage(200).send(response);
-    })
-    .cathc(error => {
-        res.status(500).send(error);
-    });
-});
-
-app.delete('/merchants/:id', (req, res) => {
-    merchant_model.deleteMerchant(req.params.id)
-    .then(response => {
-        res.status(200).send(response);
-    })
-    .catch(error => {
-        res.status(500).send(error);
-    });
-});
-
-app.listen(port, () => {
-    console.log(`server running on ${port}`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
