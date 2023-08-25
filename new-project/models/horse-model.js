c861a34c6e5ce0257ac928a2bd45cba78ea3a72a
Asalagi <mypaintedfrog@yahoo.com>
2023-08-25 10:14:04 -0500

    update horse model

--- new-project/models/horse-model.js
+++ new-project/models/horse-model.js
@@ -13,6 +13,61 @@ const getHorses = () => {
     });
 };
 
+const getHorseById = (request, response) => {
+    const id = parseInt(request.params.id);
+    pool.query('SELECT * FROM horses WHERE id = $1', [id], (error, result) => {
+        if (error) {
+            reject(error);
+        } else {
+            response(results.row);
+        }
+    });
+};
+
+const createHorse = (request, response) => {
+    const { field } = request.body
+
+    pool.query('INSERT INTO horses (field) VALUES $[number]', [field], (error, results) => {
+        if (error) {
+            reject(error);
+        } else {
+            response(results.row);
+        }
+    });
+};
+
+const updateHorse = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { field } = request.body
+    
+    pool.query('UPDATE horses SET filed = $value WHERE id = $[last number value]',
+    [filed, field, id],
+    (error, results) => {
+        if (error) {
+            reject(error);
+        } else {
+            response(results.row);
+        }
+    });
+};
+
+const deleteHorse = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM horses WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            reject(error);
+        } else {
+            response(results.row);
+        }
+    });
+};
+
+
 module.exports = {
     getHorses,
+    getHorseById,
+    createHorse,
+    updateHorse,
+
 };
