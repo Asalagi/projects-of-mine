f37f341eb946d14242c4bf98aabc3b2996bcfeb6
Asalagi <mypaintedfrog@yahoo.com>
2023-08-25 10:39:45 -0500

    add status response to horse model

--- new-project/models/horse-model.js
+++ new-project/models/horse-model.js
@@ -15,54 +15,55 @@ const getHorses = () => {
 
 const getHorseById = (request, response) => {
     const id = parseInt(request.params.id);
-    pool.query('SELECT * FROM horses WHERE id = $1', [id], (error, result) => {
+    pool.query('SELECT * FROM horses WHERE id = $1', [id], (error, results) => {
         if (error) {
-            reject(error);
+            response.status(500).json({ error: 'An error occurred' }); 
         } else {
-            response(results.row);
+            response.json(results.rows[0]); 
         }
     });
 };
 
 const createHorse = (request, response) => {
-    const { field } = request.body
+    const { name, breed, month_of_birth, year_of_birth, sex, height, color, price, notes } = request.body
 
-    pool.query('INSERT INTO horses (field) VALUES $[number]', [field], (error, results) => {
-        if (error) {
-            reject(error);
-        } else {
-            response(results.row);
-        }
-    });
+    pool.query('INSERT INTO horses (name, breed, month_of_birth, year_of_birth, sex, height, color, price, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+        [name, breed, month_of_birth, year_of_birth, sex, height, color, price, notes],
+        (error, results) => {
+            if (error) {
+                response.status(500).json({ error: 'An error occurred' }); 
+            } else {
+                response.status(201).json(results.rows[0]); 
+            }
+        });
 };
 
 const updateHorse = (request, response) => {
     const id = parseInt(request.params.id)
-    const { field } = request.body
+    const { name, breed, month_of_birth, year_of_birth, sex, height, color, price, notes } = request.body
     
-    pool.query('UPDATE horses SET filed = $value WHERE id = $[last number value]',
-    [filed, field, id],
-    (error, results) => {
-        if (error) {
-            reject(error);
-        } else {
-            response(results.row);
-        }
-    });
+    pool.query('UPDATE horses SET name = $1, breed = $2, month_of_birth = $3, year_of_birth = $4, sex = $5, height = $6, color = $7, price = $8, notes = $9 WHERE id = $10 RETURNING *',
+        [name, breed, month_of_birth, year_of_birth, sex, height, color, price, notes, id],
+        (error, results) => {
+            if (error) {
+                response.status(500).json({ error: 'An error occurred' }); 
+            } else {
+                response.json(results.rows[0]); 
+            }
+        });
 };
 
 const deleteHorse = (request, response) => {
     const id = parseInt(request.params.id)
 
-    pool.query('DELETE FROM horses WHERE id = $1', [id], (error, results) => {
+    pool.query('DELETE FROM horses WHERE id = $1 RETURNING *', [id], (error, results) => {
         if (error) {
-            reject(error);
+            response.status(500).json({ error: 'An error occurred' }); 
         } else {
-            response(results.row);
+            response.json(results.rows[0]); 
         }
     });
 };
-
 
 module.exports = {
     getHorses,
